8e4c9a49c9c5e46840e652027aac55c4aaabca84
mikkel-ol <m@ikkel.dk>
2024-04-16 17:02:20 +0200

    Remove neovim from footer

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -14,8 +14,7 @@ export const Footer: React.FC = () => {
     <div className="text-sm text-slate-500 flex flex-col gap-2">
       <p>
         Shamelessly copied from <Link href="https://github.com/bchiang7">Brittany Chiang</Link> and coded in{" "}
-        <Link href="https://neovim.io">Neovim</Link> and a little <Link href="https://code.visualstudio.com">Visual Studio Code</Link> when
-        Vim was being Vim. Built with <Link href="https://react.dev">React</Link> and{" "}
+        <Link href="https://code.visualstudio.com">Visual Studio Code</Link>. Built with <Link href="https://react.dev">React</Link> and{" "}
         <Link href="https://tailwindcss.com">Tailwind CSS</Link>, deployed with <Link href="https://www.netlify.com">Netlify</Link>.
       </p>
 
