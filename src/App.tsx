4a9bd8a43ada758b3614c3f26513c9773e5b62e1
mikkel-ol <m@ikkel.dk>
2024-04-23 21:33:58 +0200

    Add animating typing title

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import { useRef } from "react";
+import { TypeAnimation } from "react-type-animation";
 import "./App.scss";
 import { About } from "./components/about";
 import { Contacts } from "./components/contacts";
@@ -16,19 +17,35 @@ const App: React.FC = () => {
   return (
     <>
       <Searchlight />
+
       <div className="mx-auto min-h-screen max-w-screen-xl px-6 md:px-12 lg:px-24 py-12 md:py-20 lg:py-0">
         <div className="grid grid-cols-1 lg:grid-cols-2 gap-4">
           <section className="col-start-1 lg:sticky top-0 max-h-screen lg:py-24 flex flex-col justify-between">
             <header className="">
               <h1 className="text-4xl md:text-5xl text-slate-200 font-bold">Mikkel O. Laursen</h1>
 
-              <h2 className="text-xl text-slate-200 font-medium mt-3">Senior Software Engineer</h2>
+              <TypeAnimation
+                preRenderFirstString
+                sequence={[
+                  "Senior Software Developer",
+                  3000,
+                  "Senior Software Engineer",
+                  2000,
+                  "Senior Software Programmer",
+                  1000,
+                  "Senior Frontend Developer",
+                  4000,
+                  "Nerd",
+                ]}
+                speed={70}
+                className="text-xl text-slate-200 font-medium mt-3"
+                repeat={0}
+              />
 
               <p className="mt-6 mb-4 lg:mb-16 w-[75%]">
                 <span className="italic">DRY, SOLID, KISS</span> — a combination of acronyms that sounds wrong to most people, but makes
                 perfect sense to a software developer with a passion for clean code and robust architecture.
               </p>
-
               <nav className="hidden lg:block">
                 <Menu refs={[aboutRef, experienceRef, projectsRef]} />
               </nav>
