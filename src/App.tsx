3907f358cbf1b84700f9ea8d2db15a32602924de
Mikkel O. Laursen <m@ikkel.dk>
2024-12-27 10:41:14 +0100

    Update titles

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { useRef } from "react";
+import { ComponentProps, useRef } from "react";
 import { TypeAnimation } from "react-type-animation";
 import "./App.scss";
 import { About } from "./components/about";
@@ -9,10 +9,14 @@ import { Menu } from "./components/menu";
 import { Projects } from "./components/projects";
 import { Searchlight } from "./components/searchlight";
 
+type Sequence = ComponentProps<typeof TypeAnimation>["sequence"];
+
 const App: React.FC = () => {
   const aboutRef = useRef<HTMLElement>(null);
   const experienceRef = useRef<HTMLElement>(null);
   const projectsRef = useRef<HTMLElement>(null);
+
+  const roles: Sequence = ["Software Developer", 3000, "Micro Frontend Engineer", 2000, "Consultant", 2000, "Nerd"];
 
   return (
     <>
@@ -21,22 +25,12 @@ const App: React.FC = () => {
       <div className="mx-auto min-h-screen max-w-screen-xl px-6 md:px-12 lg:px-24 py-12 md:py-20 lg:py-0">
         <div className="grid grid-cols-1 lg:grid-cols-2 gap-4">
           <section className="col-start-1 lg:sticky top-0 max-h-screen lg:py-24 flex flex-col justify-between">
-            <header className="">
+            <header>
               <h1 className="text-4xl md:text-5xl text-slate-200 font-bold">Mikkel O. Laursen</h1>
 
               <TypeAnimation
                 preRenderFirstString
-                sequence={[
-                  "Senior Software Developer",
-                  3000,
-                  "Senior Software Engineer",
-                  2000,
-                  "Senior Software Programmer",
-                  1000,
-                  "Senior Frontend Developer",
-                  4000,
-                  "Nerd",
-                ]}
+                sequence={roles}
                 speed={70}
                 className="text-xl text-slate-200 font-medium mt-3"
                 repeat={0}
