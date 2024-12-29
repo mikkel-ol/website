0bf8faf819dbc7c0783cc1c65bcb9cc46e41fe48
Mikkel O. Laursen <m@ikkel.dk>
2024-12-29 13:20:13 +0100

    Add less gradient on mobile

--- src/components/searchlight.tsx
+++ src/components/searchlight.tsx
@@ -1,11 +1,14 @@
 import { useRef, useState } from "react";
 import { useKonami } from "../hooks/use-konami";
 import { usePointer } from "../hooks/use-pointer";
+import { isMobile } from "react-device-detect";
 
 export const Searchlight: React.FC = () => {
   const box = useRef<HTMLDivElement>(null);
   const [opacity, setOpacity] = useState(1);
   const { x, y } = usePointer();
+
+  const gradient = isMobile ? 300 : 600;
 
   useKonami(() => {
     setOpacity(0);
@@ -16,7 +19,7 @@ export const Searchlight: React.FC = () => {
       ref={box}
       className="fixed inset-0 z-50 pointer-events-none transition-all duration-1000"
       style={{
-        background: `radial-gradient(600px at ${x}px ${y}px, rgba(29, 78, 216, 0.15), transparent 80%)`,
+        background: `radial-gradient(${gradient}px at ${x}px ${y}px, rgba(29, 78, 216, 0.15), transparent 80%)`,
         opacity: opacity,
       }}
     ></div>
